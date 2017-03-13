5bbfc375404431bda1256a2d7f206391790249f9
Daniel <dmacrva@gmail.com>
2017-03-13 09:58:38 -0400

    catch for invalid challenge input, more precise wording

--- bin/treasure-generator.js
+++ bin/treasure-generator.js
@@ -7,13 +7,17 @@ var package = require('../package');
 program
 	.version(package.version)
 	.usage('[options] <keywords>')
-	.option('-c, --challenge <challenge>', 'Challenge rating', parseInt)
-	.option('-t, --type <type>', 'Type of treasure')
+	.option('-c, --challenge <challenge>', 'Challenge rating - integer between 0 and 20', parseInt)
+	.option('-t, --type <type>', 'Type of treasure - either "individual" or "hoard"')
 	.parse(process.argv);
 
 if (!program.challenge || !program.type) {
 	console.log('--challenge and --type are required');
 } else {
+	if (program.challenge < 0 || program.challenge > 20) {
+		console.log('challenge must be between 0 and 20');
+		process.exit(1);
+	}
 	if (program.type === 'individual' || program.type === 'hoard') {
 		console.log(roll.generateTreasure(program.challenge, program.type));
 	} else {
