9b7ecb27cfc011860c06cdbad097c6e505cc678e
Daniel <dmacrva@gmail.com>
2017-03-13 01:35:14 -0400

    finalized less QoL

--- bin/treasure-generator.js
+++ bin/treasure-generator.js
@@ -1,5 +1,22 @@
 #! /usr/bin/env node
 
 var roll = require('../lib/roll');
+var program = require('commander');
+var package = require('../package');
 
-console.log(roll.generateTreasure(14, 'hoard'));
+program
+	.version(package.version)
+	.usage('[options] <keywords>')
+	.option('-c, --challenge <challenge>', 'Challenge rating', parseInt)
+	.option('-t, --type <type>', 'Type of treasure')
+	.parse(process.argv);
+
+if (!program.challenge || !program.type) {
+	console.log('--challenge and --type are required');
+} else {
+	if (program.type === 'individual' || program.type === 'hoard') {
+		console.log(roll.generateTreasure(program.challenge, program.type));
+	} else {
+		console.log('"' + program.type + '" is not a recognized type');
+	}
+}
